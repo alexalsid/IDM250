6848afa274395376acee03765cb2b4216406c9be
alexalsid <aalsid98@gmail.com>
2018-08-15 17:08:57 -0400

    fully responsive layout

--- alpha/js/main.js
+++ alpha/js/main.js
@@ -4,27 +4,33 @@ var exitSearch = document.getElementById('exit-search');
 var search = document.getElementById('search');
 
 
+
 function openMenu () {
-    var menu = document.getElementById('nav');
+    var menu = document.getElementById('nav-mobile');
     menu.classList.remove('close');
     menu.classList.add('open');
+    body[0].classList.add('menu-open')
 };
 
 
 
 
 function closeMenu () {
-    var menu = document.getElementById('nav');
+    var menu = document.getElementById('nav-mobile');
+    var body = document.getElementsByTagName('body');
     menu.classList.remove('open');
     menu.classList.add('close');
+    body[0].classList.remove('menu-open')
 
 };
 
 function openSearch () {
 
     var searchScreen = document.getElementById('search-screen');
+    var body = document.getElementsByTagName('body');
     searchScreen.classList.remove('search');
     searchScreen.classList.add('open-search');
+    body[0].classList.add('menu-open')
 
 };
 
@@ -34,6 +40,7 @@ function closeSearch() {
     var searchScreen = document.getElementById('search-screen');
     searchScreen.classList.remove('open-search');
     searchScreen.classList.add('search');
+    body[0].classList.remove('menu-open')
 
 
 };
